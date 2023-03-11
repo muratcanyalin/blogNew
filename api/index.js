f88d3a86b6d4baa544bb369ddfc90b5c4b8ee34a
muratcanyalin <muratcanyalin07@gmail.com>
2023-03-11 17:06:04 +0300

    base kod çalışıyor complete

--- api/index.js
+++ api/index.js
@@ -74,15 +74,26 @@ app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
     const newPath = path + '.' + ext;
     fs.renameSync(path, newPath);
 
-    const { title, summary, content } = req.body;
-    const postDoc = await Post.create({
-        title,
-        summary,
-        content,
-        cover: newPath, 
-    })
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+        if (err) throw err;
 
-    res.json(postDoc);
+        const { title, summary, content } = req.body;
+        const postDoc = await Post.create({
+            title,
+            summary,
+            content,
+            cover: newPath,
+            author: info.id,
+        });
+
+        res.json(info);
+    });
+
+});
+
+app.get('/post', async (req, res) => {
+    res.json(await Post.find());
 });
 
 app.listen(4000);
--- client/src/Post.js
+++ client/src/Post.js
@@ -1,17 +1,20 @@
-export default function Post() {
-    return ( 
+import { formatISO9075 } from "date-fns"
+
+export default function Post({ title, summary, cover, content, createdAt }) {
+
+    return (
         <div className='post'>
             <div className='image'>
                 <img src='https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=730&crop=1' alt='' />
             </div>
             <div className='texts'>
-                <h2>full-house battery backup coming later this year</h2>
+                <h2>{title}</h2>
                 <p className='info'>
                     <a href='author'>Dawid Paszko</a>
-                    <time>2023-01-06 16:45</time>
+                    <time>{formatISO9075(new Date(createdAt))}</time>
                 </p>
                 <p className='summary'>
-                    Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.”
+                    {summary}
                 </p>
             </div>
         </div>
