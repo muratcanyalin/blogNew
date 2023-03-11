b480500fcb2a29f56052905d87dbabd03e833e09
muratcanyalin <muratcanyalin07@gmail.com>
2023-03-11 16:22:04 +0300

    mongoDB post gönderimi, file upload sorunsuz

--- api/index.js
+++ api/index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const { mongoose } = require('mongoose');
 const User = require('./models/User');
+const Post = require('./models/Post');
 const bcrypt = require('bcryptjs');
 const app = express();
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
+const multer = require('multer');
+const uploadMiddleware = multer({ dest: 'uploads/' });
+const fs = require('fs');
+
 
 const salt = bcrypt.genSaltSync(10);
 const secret = 'ASDOFWOEIF3443NOFG43';
@@ -39,11 +44,11 @@ app.post('/login', async (req, res) => {
         jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
             if (err) throw err;
             res.cookie('token', token).json({
-                id:userDoc._id,
+                id: userDoc._id,
                 username,
             });
 
-        }); 
+        });
     }
     else {
         res.status(400).json('wrong credentials');
@@ -58,9 +63,27 @@ app.get('/profile', (req, res) => {
     });
 });
 
-app.post('/logout', (req,res) => {
+app.post('/logout', (req, res) => {
     res.cookie('token', '').json('ok');
-})
+});
+
+app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
+    const { originalname, path } = req.file;
+    const parts = originalname.split('.');
+    const ext = parts[parts.length - 1];
+    const newPath = path + '.' + ext;
+    fs.renameSync(path, newPath);
+
+    const { title, summary, content } = req.body;
+    const postDoc = await Post.create({
+        title,
+        summary,
+        content,
+        cover: newPath, 
+    })
+
+    res.json(postDoc);
+});
 
 app.listen(4000);
 //  
--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -22,9 +22,24 @@ export default function CreatePost() {
     const [title, setTitle] = useState('');
     const [summary, setSummary] = useState('');
     const [content, setContent] = useState('');
+    const [files, setFiles] = useState('');
+    async function createNewPost(ev) {
+        const data = new FormData();
+        data.set('title', title);
+        data.set('summary', summary);
+        data.set('content', content);
+        data.set('file', files[0]);
+        ev.preventDefault(); 
+        const response = await fetch('http://localhost:4000/post', {
+            method: 'POST',
+            body: data, 
+
+        });
+        console.log(await response.json());
+    }
 
     return (
-        <form>
+        <form onSubmit={createNewPost}>
             <input type="title"
                 placeholder={'Title'}
                 value={title}
@@ -33,7 +48,8 @@ export default function CreatePost() {
                 placeholder={'Summary'}
                 value={summary}
                 onChange={ev => setSummary(ev.target.value)} />
-            <input type="file" />
+            <input type="file" 
+            onChange={ev => setFiles(ev.target.files)}  />
             <ReactQuill value={content} 
             onChange={newValue => setContent(newValue)} 
             modules={modules} 
