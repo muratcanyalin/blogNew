dc72fa0f9aa7b574300f04b90994ac3c114c2dc0
muratcanyalin <muratcanyalin07@gmail.com>
2023-03-13 14:21:54 +0300

    re upload

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { mongoose } = require('mongoose');
+const mongoose = require("mongoose");
 const User = require('./models/User');
 const Post = require('./models/Post');
 const bcrypt = require('bcryptjs');
@@ -11,9 +11,8 @@ const multer = require('multer');
 const uploadMiddleware = multer({ dest: 'uploads/' });
 const fs = require('fs');
 
-
 const salt = bcrypt.genSaltSync(10);
-const secret = 'ASDOFWOEIF3443NOFG43';
+const secret = 'asdfe45we45w345wegw345werjktjwertkj';
 
 app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 app.use(express.json());
@@ -21,8 +20,6 @@ app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
 
 mongoose.connect('mongodb+srv://blog:3E26CbwMfIzTDotK@cluster0.iokndqh.mongodb.net/?retryWrites=true&w=majority');
-
-
 
 app.post('/register', async (req, res) => {
     const { username, password } = req.body;
@@ -43,17 +40,15 @@ app.post('/login', async (req, res) => {
     const userDoc = await User.findOne({ username });
     const passOk = bcrypt.compareSync(password, userDoc.password);
     if (passOk) {
-        //logged in
+        // logged in
         jwt.sign({ username, id: userDoc._id }, secret, {}, (err, token) => {
             if (err) throw err;
             res.cookie('token', token).json({
                 id: userDoc._id,
                 username,
             });
-
         });
-    }
-    else {
+    } else {
         res.status(400).json('wrong credentials');
     }
 });
@@ -80,7 +75,6 @@ app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
     const { token } = req.cookies;
     jwt.verify(token, secret, {}, async (err, info) => {
         if (err) throw err;
-
         const { title, summary, content } = req.body;
         const postDoc = await Post.create({
             title,
@@ -88,6 +82,36 @@ app.post('/post', uploadMiddleware.single('file'), async (req, res) => {
             content,
             cover: newPath,
             author: info.id,
+        });
+        res.json(postDoc);
+    });
+
+});
+
+app.put('/post', uploadMiddleware.single('file'), async (req, res) => {
+    let newPath = null;
+    if (req.file) {
+        const { originalname, path } = req.file;
+        const parts = originalname.split('.');
+        const ext = parts[parts.length - 1];
+        newPath = path + '.' + ext;
+        fs.renameSync(path, newPath);
+    }
+
+    const { token } = req.cookies;
+    jwt.verify(token, secret, {}, async (err, info) => {
+        if (err) throw err;
+        const { id, title, summary, content } = req.body;
+        const postDoc = await Post.findById(id);
+        const isAuthor = JSON.stringify(postDoc.author) === JSON.stringify(info.id);
+        if (!isAuthor) {
+            return res.status(400).json('you are not the author');
+        }
+        await postDoc.update({
+            title,
+            summary,
+            content,
+            cover: newPath ? newPath : postDoc.cover,
         });
 
         res.json(postDoc);
@@ -106,8 +130,9 @@ app.get('/post', async (req, res) => {
 
 app.get('/post/:id', async (req, res) => {
     const { id } = req.params;
-    const postDoc = await Post.findById(id).populate();
+    const postDoc = await Post.findById(id).populate('author', ['username']);
     res.json(postDoc);
 })
+
 app.listen(4000);
-//  
+//
--- client/src/Layout.js
+++ client/src/Layout.js
@@ -1,11 +1,11 @@
 import Header from "./Header";
-import { Outlet } from "react-router-dom";
+import {Outlet} from "react-router-dom";
 
 export default function Layout() {
-    return (
-        <main>
-            <Header />
-            <Outlet />
-        </main>
-    );
+  return (
+    <main>
+      <Header />
+      <Outlet />
+    </main>
+  );
 }
